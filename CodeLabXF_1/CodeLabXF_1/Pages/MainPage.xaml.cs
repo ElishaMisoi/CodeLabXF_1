1fbd88c1ed03dc078531e8fbce14ff35df5b1101
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-01-11 12:24:11 +0300

    Added Share Functionality

--- CodeLabXF_1/CodeLabXF_1/Pages/MainPage.xaml.cs
+++ CodeLabXF_1/CodeLabXF_1/Pages/MainPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,13 @@ namespace CodeLabXF_1.Pages
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var selected_item = e.Item as Models.User;
+
+            await Browser.OpenAsync(selected_item.html_url, BrowserLaunchMode.SystemPreferred);
         }
     }
 }
